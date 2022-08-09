f59420739529c1ff4919aedabc601732ab30583a
hyunkwanjeong <jhg728@naver.com>
2022-08-09 19:01:38 +0900

    feat: leetcode22 start

---
+++ src/main/leetcode/dp/LeetCode22_GenerateParentheses.java
@@ -0,0 +1,14 @@
+package leetcode.dp;
+
+import java.util.List;
+
+public class LeetCode22_GenerateParentheses {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static List<String> generateParenthesis(int n) {
+        return null;
+    }
+}
