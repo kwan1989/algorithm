2027ea76ede11ac5b4120dcc0e0c9a115b5098da
hyunkwanjeong <jhg728@naver.com>
2023-06-05 16:23:06 +0900

    feat: modify 1. LeetCode05_LongestPalindromicSubstring

--- src/main/leetcode/dp/LeetCode05_LongestPalindromicSubstring.java
+++ src/main/leetcode/dp/LeetCode05_LongestPalindromicSubstring.java
@@ -45,38 +45,4 @@ public class LeetCode05_LongestPalindromicSubstring {
         return s.substring(left + 1, right);
     }
 
-
-//    public static String longestPalindrome(String s) {
-//        if (s == null || s.length() < 2) {
-//            return s;
-//        }
-//
-//        String longest = "";
-//
-//        for (int i = 0; i < s.length(); i++) {
-//            String palindrome = expandAroundCenter(s, i, i);
-//
-//            if (palindrome.length() > longest.length()) {
-//                longest = palindrome;
-//            }
-//
-//        }
-//        return longest;
-//    }
-//
-//    private static String expandAroundCenter(String s, int left, int right) {
-//
-//        // s 의 길이가 짝수인경우 [1,1,1,1] 중앙 시작점을 찾기 어렵다. 그러므로 우측열 +1
-//        if(s.length() % 2 == 0){
-//            right++;
-//        }
-//
-//        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-//            left--;
-//            right++;
-//        }
-//
-//        System.out.println(s.substring(left + 1, right));
-//        return s.substring(left + 1, right);
-//    }
 }
