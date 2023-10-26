50d6bbe8e2c815044e903e06f5270598d0a0556a
kwan1989 <jhg728@naver.com>
2023-10-26 20:57:14 +0900

    feat: twopoiont 1. LeetCode15_3Sum

--- src/main/leetcode/etc/LeetCode15_3Sum.java
+++ src/main/leetcode/etc/LeetCode15_3Sum.java
@@ -6,11 +6,37 @@ public class LeetCode15_3Sum {
     public static void main(String[] args) {
         int[] nums = {-1, 0, 1, 2, -1, -4};
 
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        result = threeSum(nums);
+//        List<List<Integer>> result = new ArrayList<List<Integer>>();
+//        result = threeSum(nums);
+
+        System.out.println(threeSum(nums));
     }
-    
+
     public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        Set<List<Integer>> result = new HashSet<>();
+
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                } else if(sum < 0){
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return new ArrayList<>(result);
+    }
+
+    public static List<List<Integer>> threeSum1(int[] nums) {
         Set<List<Integer>> threeSumSet = new HashSet<>();
         Arrays.sort(nums);
 
