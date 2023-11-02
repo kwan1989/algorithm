6fa55524917ec708c93788354bef36d1b38936b5
kwan1989 <jhg728@naver.com>
2023-11-03 05:51:10 +0900

    feat: 문제 복기(재풀이) 1. LeetCode94_BinaryTreeInorderTraversal

--- src/main/leetcode/dfs/LeetCode94_BinaryTreeInorderTraversal.java
+++ src/main/leetcode/dfs/LeetCode94_BinaryTreeInorderTraversal.java
@@ -35,31 +35,30 @@ public class LeetCode94_BinaryTreeInorderTraversal {
     }
 
     public static List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        dfs(root, res);
-        return res;
+        List<Integer> result = new ArrayList<>();
+        dfs(root, result);
+        return result;
     }
 
-    public static void dfs(TreeNode root, List<Integer> res) {
+    public static void dfs(TreeNode root, List<Integer> result) {
         if (root != null) {
+            return;
+        }
+        // 전위
+        if (root.left != null) {
+            dfs(root.left, result);
+        }
 
-            // 전위
-            if (root.left != null) {
-                dfs(root.left, res);
-            }
+        /*
+            중위
+            전위 앞에 있으면 전위 표기법
+            후위 뒤에 있으면 후위 표기법
+         */
+        result.add(root.val);
 
-            /*
-                중위
-                전위 앞에 있으면 전위 표기법
-                후위 뒤에 있으면 후위 표기법
-             */
-            res.add(root.val);
-
-            // 후위
-            if (root.right != null) {
-                dfs(root.right, res);
-            }
-
+        // 후위
+        if (root.right != null) {
+            dfs(root.right, result);
         }
     }
 }
