c5bf8a943889a384caf56b4f5824a161cadd515b
hyunkwanjeong <>
2023-11-02 21:42:27 +0900

    feat: 문제 복기(재풀이) 1. LeetCode34_FindFirstandLastPositionofElementinSortedArray

--- src/main/leetcode/binarysearch/LeetCode34_FindFirstandLastPositionofElementinSortedArray.java
+++ src/main/leetcode/binarysearch/LeetCode34_FindFirstandLastPositionofElementinSortedArray.java
@@ -6,11 +6,11 @@ import java.util.Collections;
 public class LeetCode34_FindFirstandLastPositionofElementinSortedArray {
 
     public static void main(String[] args) {
-//        int[] nums = {5, 7, 7, 8, 8, 10};
-//        int target = 8;
-
         int[] nums = {5, 7, 7, 8, 8, 10};
-        int target = 6;
+        int target = 8;
+
+//        int[] nums = {5, 7, 7, 8, 8, 10};
+//        int target = 6;
 
 //        int[] nums = {};
 //        int target = 0;
@@ -22,58 +22,39 @@ public class LeetCode34_FindFirstandLastPositionofElementinSortedArray {
     }
 
     public static int[] searchRange(int[] nums, int target) {
+        return new int[]{binarySearch(nums, target, FIRST), binarySearch(nums, target, LAST)};
+    }
 
-        int result[] = new int[2];
-        result[0] =findStart(nums,target);
-        result[1] =findEnd(nums,target);
+    static final int FIRST = 0;
+    static final int LAST = 1;
 
+    public static int binarySearch(int[] nums, int target, int direction) {
+        int start = 0;
+        int end = nums.length - 1;
+        int result = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                result = mid;
+            }
+
+            if (direction == FIRST) {
+                if (nums[mid] >= target) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
         return result;
-    }
-
-    public static int findStart(int[] nums, int target){
-        int idx = -1;
-        int start = 0;
-        int end = nums.length - 1;
-
-        while (start <= end) {
-
-            int mid = (start + end) / 2;
-
-            if (nums[mid] >= target) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-
-            if ( nums[mid] == target) {
-                idx =  mid;
-            }
-        }
-
-        return idx;
-    }
-
-    public static int findEnd(int[] nums, int target){
-        int idx = -1;
-        int start = 0;
-        int end = nums.length - 1;
-
-        while (start <= end) {
-
-            int mid = (start + end) / 2;
-
-            if (nums[mid] <= target) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-
-            if ( nums[mid] == target) {
-                idx =  mid;
-            }
-        }
-
-        return idx;
     }
 
 }
