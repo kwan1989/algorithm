84d18ea9f2928eb9bb6208355e09f8088e998c8e
kwan1989 <jhg728@naver.com>
2023-11-03 18:38:08 +0900

    feat: LeetCode 278 1. LeetCode278_FirstBadVersion

---
+++ src/main/leetcode/binarysearch/LeetCode278_FirstBadVersion.java
@@ -0,0 +1,25 @@
+package leetcode.binarysearch;
+
+public class LeetCode278_FirstBadVersion {
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    public static int firstBadVersion(int n) {
+//        int left = 0;
+//        int right = n;
+//        while(left < right){
+//            int mid = left +(right-left) / 2;
+//            if (isBadVersion(mid)){
+//                right = mid;
+//            } else {
+//                left = mid + 1;
+//            }
+//        }
+//        return left;
+//    }
+}
