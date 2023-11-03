88be38f51ab170df73ae6c5291ef006a494a13fb
kwan1989 <jhg728@naver.com>
2023-11-03 19:02:24 +0900

    feat: LeetCode 383 1. LeetCode383_RansomNote

---
+++ src/main/leetcode/hashmap/LeetCode383_RansomNote.java
@@ -0,0 +1,32 @@
+package leetcode.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LeetCode383_RansomNote {
+
+    public static void main(String[] args) {
+        System.out.println(canConstruct("aa", "aab"));
+    }
+
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : magazine.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) +1);
+        }
+
+        for (char c : ransomNote.toCharArray()){
+            if (map.containsKey(c)){
+                if(map.get(c)-1 > 0){
+                    map.put(c, map.get(c)-1);
+                } else {
+                    map.remove(c);
+                }
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
