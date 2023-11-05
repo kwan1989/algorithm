ec9994248b5ea35176eaeaeb60e1cd0ca22e0213
kwan1989 <jhg728@naver.com>
2023-11-05 23:07:53 +0900

    feat: LeetCode 57, 542 1. LeetCode57_InsertInterval 2. LeetCode542_01Matrix

---
+++ src/main/leetcode/array/LeetCode57_InsertInterval.java
@@ -0,0 +1,63 @@
+package leetcode.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 이 문제 어렵다 다시 풀어야함.
+ * 기존의 구간 배열 (intervals) :
+ * [1, 2], [3, 5], [6, 7], [8, 10], [12, 16]
+ *
+ * 새로운 구간 (newInterval) :
+ * [4, 8]
+ *
+ * 1. 새로운 구간의 시작점보다 끝점이 먼저인 구간들을 결과 목록에 추가합니다.
+ * 결과 목록 :
+ * [1, 2]
+ *
+ * 2. 새로운 구간과 겹치는 모든 구간을 찾아서 병합합니다.
+ * - [3, 5]와 [4, 8]은 겹치므로 병합하여 [3, 8]를 얻습니다.
+ * - [3, 8]과 [6, 7]은 겹치므로 병합하여 [3, 8]를 얻습니다.
+ * - [3, 8]과 [8, 10]은 겹치므로 병합하여 [3, 10]를 얻습니다.
+ * 병합된 구간을 결과 목록에 추가합니다.
+ * 결과 목록 :
+ * [1, 2], [3, 10]
+ *
+ * 3. 나머지 구간들을 결과 목록에 추가합니다.
+ * 결과 목록 :
+ * [1, 2], [3, 10], [12, 16]
+ */
+public class LeetCode57_InsertInterval {
+
+    public static void main(String[] args) {
+//        System.out.println(Arrays.deepToString(insert(new int[][]{{1, 3}, {6, 9}}, new int[]{2, 5})));
+        System.out.println(Arrays.deepToString(insert(new int[][]{{1,2},{3,5},{6,7},{8,10},{12,16}}, new int[]{4,8})));
+    }
+
+    public static int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> result = new ArrayList<>();
+        int i = 0;
+
+        // Add all the intervals ending before newInterval starts
+        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i++]);
+        }
+
+        // Merge all overlapping intervals to one considering newInterval
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        result.add(newInterval); // Add the union of intervals we got
+
+        // Add all the rest
+        while (i < intervals.length) {
+            result.add(intervals[i++]);
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+
+}
