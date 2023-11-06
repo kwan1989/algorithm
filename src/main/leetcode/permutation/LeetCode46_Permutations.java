07dd7baec1312d72ae656f14466bcfd7d19dbe50
hyunkwanjeong <jhg728@naver.com>
2023-11-06 22:30:59 +0900

    feat: LeetCode 46 (permutation) 1. LeetCode46_Permutations

--- src/main/leetcode/permutation/LeetCode46_Permutations.java
+++ src/main/leetcode/permutation/LeetCode46_Permutations.java
@@ -21,22 +21,45 @@ public class LeetCode46_Permutations {
             permutation(nums, new ArrayList<>(), result, length);
         }
 
+
+        for (int length = 1; length <= nums.length; length++) {
+            permutation_중복숫자가능(nums, 0,new ArrayList<>(), result, length);
+        }
+
         return result;
     }
 
     public static void permutation(int[] nums,
                                    List<Integer> curr,
                                    List<List<Integer>> result,
-                                   int length) {
+                                   int length)
+    {
         if (curr.size() == length) {
             result.add(new ArrayList<>(curr));
         } else {
-            for (int num : nums) {
-                if (!curr.contains(num)) {
-                    curr.add(num);
+            for (int i = 0; i<nums.length; i++){
+                if(!curr.contains(nums[i])){
+                    curr.add(nums[i]);
                     permutation(nums, curr, result, length);
                     curr.remove(curr.size() - 1);
                 }
+            }
+        }
+    }
+
+    public static void permutation_중복숫자가능(int[] nums,
+                                   int index,
+                                   List<Integer> curr,
+                                   List<List<Integer>> result,
+                                   int length)
+    {
+        if(curr.size() == length){
+            result.add(new ArrayList<>(curr));
+        } else {
+            for (int i = index; i<nums.length; i++){
+                curr.add(nums[i]);
+                permutation_중복숫자가능(nums, i, curr, result, length);
+                curr.remove(curr.size() - 1);
             }
         }
     }
