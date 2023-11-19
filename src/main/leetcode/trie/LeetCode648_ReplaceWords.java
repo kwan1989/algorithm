9dcc28acf8c3037a171b6b32fd5799784dd88d11
hyunkwanjeong <jhg728@naver.com>
2023-11-19 20:42:34 +0900

    feat: 648 class name

--- src/main/leetcode/trie/LeetCode648_ReplaceWords.java
+++ src/main/leetcode/trie/LeetCode648_ReplaceWords.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class LeetCdoe648_ReplaceWords {
+public class LeetCode648_ReplaceWords {
 
     public static void main(String[] args) {
         List<String> dictionary1 = Arrays.asList("cat", "bat", "rat");
