97d1c14f901bf16887f702b11eb2f5243c4d7434
hyunkwanjeong <jhg728@naver.com>
2024-04-05 15:18:19 +0900

    feat. leetCode Retry 1. LeetCode242_ValidAnagram

--- src/main/leetcode/hashmap/LeetCode242_ValidAnagram.java
+++ src/main/leetcode/hashmap/LeetCode242_ValidAnagram.java
@@ -6,8 +6,8 @@ import java.util.Map;
 public class LeetCode242_ValidAnagram {
 
     public static void main(String[] args) {
-        System.out.println(isAnagram("anagram", "nagaram"));
-        System.out.println(isAnagram("rat", "car"));
+//        System.out.println(isAnagram1("anagram", "nagaram"));
+        System.out.println(isAnagram1("rat", "car"));
     }
 
     public static boolean isAnagram(String s, String t) {
@@ -21,13 +21,39 @@ public class LeetCode242_ValidAnagram {
             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
         }
 
-        for (int j =0; j<t.length(); j++){
-            if (map.containsKey(t.charAt(j))){
-                map.put(t.charAt(j), map.get(t.charAt(j)) -1);
+        for (int j = 0; j < t.length(); j++) {
+            if (map.containsKey(t.charAt(j))) {
+                map.put(t.charAt(j), map.get(t.charAt(j)) - 1);
 
-                if (map.get(t.charAt(j)) == 0){
+                if (map.get(t.charAt(j)) == 0) {
                     map.remove(t.charAt(j));
                 }
+            }
+        }
+
+        return map.isEmpty();
+    }
+
+    public static boolean isAnagram1(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : t.toCharArray()){
+            if(map.containsKey(c)){
+                map.put(c, map.get(c) - 1);
+            } else {
+                return false;
+            }
+
+            if (map.get(c) == 0 ){
+                map.remove(c);
             }
         }
 
