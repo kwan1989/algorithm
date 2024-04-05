8004cd39c809f53043e479750552dc5b566becc0
hyunkwanjeong <jhg728@naver.com>
2024-04-05 16:41:45 +0900

    feat. leetCode Retry 1. LeetCode383_RansomNote

--- src/main/leetcode/hashmap/LeetCode383_RansomNote.java
+++ src/main/leetcode/hashmap/LeetCode383_RansomNote.java
@@ -29,4 +29,28 @@ public class LeetCode383_RansomNote {
         }
         return true;
     }
+
+    public static boolean canConstruct_1(String ransomNote, String magazine) {
+        Map<Character, Integer> map =new HashMap<>();
+
+        for (char c : magazine.toCharArray()){
+            map.put(c, map.getOrDefault(c,0) + 1);
+        }
+
+        for (char c: ransomNote.toCharArray()){
+            if(map.containsKey(c)){
+                map.put(c, map.get(c) -1);
+
+                if (map.get(c) < 0){
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
 }
