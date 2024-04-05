c2ef14ea3c2a464ce61704410dfc3a8ce6f12064
hyunkwanjeong <jhg728@naver.com>
2024-04-05 15:38:14 +0900

    feat. leetCode Retry 1. LeetCode704_BinarySearch

---
+++ src/main/leetcode/binarysearch/LeetCode704_BinarySearch.java
@@ -0,0 +1,28 @@
+package leetcode.binarysearch;
+
+public class LeetCode704_BinarySearch {
+
+    public static void main(String[] args) {
+        System.out.println(search(new int[]{-1, 0, 3, 5, 9, 12}, 2));
+    }
+
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
