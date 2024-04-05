d6f830a3aade403261df3dd318e5de68020b42ce
hyunkwanjeong <jhg728@naver.com>
2024-04-05 16:36:51 +0900

    feat. leetCode Retry 1. LeetCode278_FirstBadVersion

--- src/main/leetcode/binarysearch/LeetCode278_FirstBadVersion.java
+++ src/main/leetcode/binarysearch/LeetCode278_FirstBadVersion.java
@@ -1,25 +1,44 @@
 package leetcode.binarysearch;
 
 public class LeetCode278_FirstBadVersion {
-/* The isBadVersion API is defined in the parent class VersionControl.
-      boolean isBadVersion(int version); */
+//The isBadVersion API is defined in the parent class VersionControl.
+     public static  boolean isBadVersion(int version){
+         return true;
+     }
 
 
-//    public static void main(String[] args) {
-//
-//    }
-//
-//    public static int firstBadVersion(int n) {
-//        int left = 0;
-//        int right = n;
-//        while(left < right){
-//            int mid = left +(right-left) / 2;
-//            if (isBadVersion(mid)){
-//                right = mid;
-//            } else {
-//                left = mid + 1;
-//            }
-//        }
-//        return left;
-//    }
+    public static void main(String[] args) {
+
+    }
+
+    public static int firstBadVersion(int n) {
+        int left = 0;
+        int right = n;
+        while(left < right){
+            int mid = left +(right-left) / 2;
+            if (isBadVersion(mid)){
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public static int firstBadVersion_1(int n) {
+         int left = 0;
+         int right = n;
+
+         while( left < right){
+             int mid = left + (right-left) / 2;
+
+             if(isBadVersion(mid)){
+                 right = mid;
+             } else{
+                 left = mid + 1;
+             }
+         }
+
+         return left;
+    }
 }
