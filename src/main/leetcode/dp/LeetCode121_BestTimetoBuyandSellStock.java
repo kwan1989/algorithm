93179290b7745426084edd518ed7813af5efa4c7
hyunkwanjeong <jhg728@naver.com>
2024-05-07 11:15:26 +0900

    feat. retry 1. LeetCode121_BestTimetoBuyandSellStock

--- src/main/leetcode/dp/LeetCode121_BestTimetoBuyandSellStock.java
+++ src/main/leetcode/dp/LeetCode121_BestTimetoBuyandSellStock.java
@@ -64,4 +64,16 @@ public class LeetCode121_BestTimetoBuyandSellStock {
         }
         return max;
     }
+
+    public static int maxProfit_2(int[] prices) {
+        int min = prices[0];
+        int max = 0;
+
+        for (int i = 1; i< prices.length; i++){
+            min = Math.min(min, prices[i]);
+            max = Math.max(max, prices[i] - min);
+        }
+
+        return max;
+    }
 }
