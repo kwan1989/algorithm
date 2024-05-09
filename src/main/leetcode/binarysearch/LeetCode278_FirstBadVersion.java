55fe34dbcbbb09d86e606a0aa2451c7399b6cd12
hyunkwanjeong <jhg728@naver.com>
2024-05-09 15:15:48 +0900

    feat. LeetCode retry ref. 1. LeetCode278_FirstBadVersion

--- src/main/leetcode/binarysearch/LeetCode278_FirstBadVersion.java
+++ src/main/leetcode/binarysearch/LeetCode278_FirstBadVersion.java
@@ -1,10 +1,10 @@
 package leetcode.binarysearch;
 
 public class LeetCode278_FirstBadVersion {
-//The isBadVersion API is defined in the parent class VersionControl.
-     public static  boolean isBadVersion(int version){
-         return true;
-     }
+    //The isBadVersion API is defined in the parent class VersionControl.
+    public static boolean isBadVersion(int version) {
+        return true;
+    }
 
 
     public static void main(String[] args) {
@@ -14,9 +14,9 @@ public class LeetCode278_FirstBadVersion {
     public static int firstBadVersion(int n) {
         int left = 0;
         int right = n;
-        while(left < right){
-            int mid = left +(right-left) / 2;
-            if (isBadVersion(mid)){
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
                 right = mid;
             } else {
                 left = mid + 1;
@@ -25,20 +25,26 @@ public class LeetCode278_FirstBadVersion {
         return left;
     }
 
+    /**
+     * 특정 mid 값을 찾는게 아니라면, 만족하는 첫번째 요소를 검색하는 것이 목적이다,
+     * right = mid - 1 을 사용하지 않는건, mid 가 해당 요소 일수 있으므로
+     * @param n
+     * @return
+     */
     public static int firstBadVersion_1(int n) {
-         int left = 0;
-         int right = n;
+        int left = 0;
+        int right = n;
 
-         while( left < right){
-             int mid = left + (right-left) / 2;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
 
-             if(isBadVersion(mid)){
-                 right = mid;
-             } else{
-                 left = mid + 1;
-             }
-         }
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
 
-         return left;
+        return left;
     }
 }
