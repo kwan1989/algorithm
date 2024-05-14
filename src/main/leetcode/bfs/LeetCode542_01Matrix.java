3567a94668d4ab28b1111ba41cf1db7583142e6c
hyunkwanjeong <jhg728@naver.com>
2024-05-14 12:41:18 +0900

    feat. LeetCode retry 1. LeetCode542_01Matrix

--- src/main/leetcode/bfs/LeetCode542_01Matrix.java
+++ src/main/leetcode/bfs/LeetCode542_01Matrix.java
@@ -8,7 +8,8 @@ public class LeetCode542_01Matrix {
 
     public static void main(String[] args) {
         int[][] matrix = {{0, 0, 0}, {0, 1, 0}, {1, 1, 1}};
-        System.out.println(Arrays.deepToString(updateMatrix(matrix)));
+//        System.out.println(Arrays.deepToString(updateMatrix(matrix)));
+        System.out.println(Arrays.deepToString(updateMatrix_2(matrix)));
     }
 
     public static int[][] updateMatrix(int[][] mat) {
@@ -80,5 +81,40 @@ public class LeetCode542_01Matrix {
         }
         return mat;
 
+    }
+
+
+    public static int[][] updateMatrix_2(int[][] mat) {
+        boolean[][] visited = new boolean[mat.length][mat[0].length];
+        Queue<int[]> queue = new LinkedList<>();
+        int[][] DIRS = {{-1, 0}, {1, 0}, {-1, 0}, {0, 1}};
+
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (mat[i][j] == 0) {
+                    visited[i][j] = true;
+                    queue.offer(new int[]{i, j});
+                }
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+
+            for (int[] DIR : DIRS) {
+                int nextRow = curr[0] + DIR[0];
+                int nextCol = curr[1] + DIR[1];
+
+                if (nextRow >= 0 && nextRow < mat.length
+                        && nextCol >= 0 && nextCol < mat[0].length
+                        && !visited[nextRow][nextCol]) {
+                    visited[nextRow][nextCol] = true;
+                    mat[nextRow][nextCol] = mat[curr[0]][curr[1]] + 1;
+                    queue.offer(new int[]{nextRow, nextCol});
+                }
+            }
+        }
+        return mat;
     }
 }
